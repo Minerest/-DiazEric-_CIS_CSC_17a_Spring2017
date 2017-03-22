15cfef14230bb26e7515a81850acdff0aab661d1
Minerest <Eric.diaz13@yahoo.com>
2017-03-22 11:45:56 -0700

    Signed-off-by: Minerest <Eric.diaz13@yahoo.com>

--- lab/structAssn/main.cpp
+++ lab/structAssn/main.cpp
@@ -1,3 +1,11 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on March 8th, 2017, 1:30 PM
+ * Purpose:  Database sort
+ */
+
+//System Libraries Here
 #include <iostream>
 using namespace std;
 
@@ -42,12 +50,12 @@ void destroy(Array *a){
 }
 
 void markSrt(Array *a){
-    for(int i=0;i<(*a).size-1;i++){
-        for(int j=i+1;j<(*a).size;j++){
-            if((*a).data[(*(*a).index+i)]>(*a).data[*((*a).index+j)]){
-                int temp=(*(*a).index+i);
-                (*a).index[i]=*(*a).index+j;//This one is tricky. Can't take 
-                (*a).index[j]=temp;         //out the left hand []
+    for(int i=0; i<a->size-1;i++){
+        for(int j=i+1;j<a->size;j++){
+            if(*((*a).data+*((*a).index+i))>*((*a).data + *((*a).index+j))){
+                int temp=*((*a).index+i);
+                *((*a).index+i)= *((*a).index+j);
+                *((*a).index+j)=temp;
             }
         }
     }
@@ -57,7 +65,7 @@ void markSrt(Array *a){
 void prntAry(Array *a,int perLine){
     cout<<endl;
     for(int i=0;i<(*a).size;i++){
-        cout<<(*a).data[(*(*a).index+i)];
+        cout<<*((*a).data + *((*a).index+i));
         if(i%perLine==(perLine-1))cout<<endl;
     }
     cout<<endl;
@@ -67,13 +75,13 @@ void prntAry(Array *a,int perLine){
 Array *fillAry(int n,int mod){
     //Declare the array and its components
     Array *a=new Array;
-    (*a).size=n>1?n:2;
-    (*a).data=new int[(*a).size];
-    (*a).index=new int[(*a).size];
+    a->size=n>1?n:2;
+    a->data=new int[a->size];// leave these alone, right?
+    a->index=new int[a->size];
     //Fill the components with data
-    for(int i=0;i<(*a).size;i++){
-        (*a).data[i]=i%mod;
-        (*a).index[i]=i;
+    for(int i=0;i<a->size;i++){
+        *((*a).data+i)=i%mod;
+        *((*a).index+i)=i;
     }
     //Return the Array
     return a;
